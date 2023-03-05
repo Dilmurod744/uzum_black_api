d2d5ea51acdf4d37d794faf488531205af29ed8c
Justeee <justeee03@gmail.com>
2023-03-05 19:55:28 +0500

    permissions

--- apps/ecommerce/admin.py
+++ apps/ecommerce/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 from mptt.admin import DraggableMPTTAdmin
 
 from apps.ecommerce.models import Category, Shop, Product, Cart, Wishlist, Order
@@ -10,7 +11,18 @@ class CategoryMPTTModelAdmin(DraggableMPTTAdmin):
 
 
 admin.site.register(Shop)
-admin.site.register(Product)
 admin.site.register(Cart)
 admin.site.register(Wishlist)
 admin.site.register(Order)
+
+
+class ProductAdmin(ModelAdmin):
+    def get_queryset(self, request):
+        # if request.user.is_superuser:
+        #     return super(ProductAdmin, self).get_queryset(request)
+        # else:
+            qs = super(ProductAdmin, self).get_queryset(request)
+            return qs.filter(owner=request.user)
+
+
+admin.site.register(Product, ProductAdmin)
